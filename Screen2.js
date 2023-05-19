a4d35f1da533e5dccf0d539699afefacc960d0d2
AyushKatochh <katochayushhp@gmail.com>
2023-05-19 23:53:04 +0530

    update

---
+++ Screen2.js
@@ -0,0 +1,70 @@
+import React, { useEffect } from 'react';
+import { View, Text, ImageBackground, Image, StyleSheet } from 'react-native';
+
+const Screen2 = ({ navigation }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      navigation.navigate('Screen3');
+    }, 20000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <ImageBackground source={require('./award_bg.png')} style={styles.container}>
+      <Image source={require('./curtain.png')} style={[styles.curtain, styles.curtainLeft]} resizeMode="contain" />
+      <Image source={require('./curtain.png')} style={[styles.curtain, styles.curtainRight]} resizeMode="contain" />
+      <View style={styles.contentContainer}>
+        <Image source={require('./main-heart.png')} style={styles.imageAbove} resizeMode="contain" />
+        <Text style={styles.screenText}>Screen 2</Text>
+        <Image source={require('./girlClap.png')} style={styles.image} resizeMode="contain" />
+        <Image source={require('./awardPlatform.png')} style={styles.imageBelow} resizeMode="contain" />
+      </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  curtain: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    width: 100,
+  },
+  curtainLeft: {
+    left: 0,
+  },
+  curtainRight: {
+    right: 0,
+  },
+  contentContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageAbove: {
+    width: 100,
+    height: 200,
+    marginBottom: 20,
+  },
+  screenText: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginTop: 20,
+  },
+  imageBelow: {
+    width: 400,
+    height: 200,
+    marginTop: 20,
+  },
+});
+
+export default Screen2;
