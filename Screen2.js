1f9d9cc64c8c40c3181938a80a08c13e1bfc2f24
Ayush Katoch <katochayushhp@gmail.com>
2023-05-21 11:57:38 +0530

    Update Screen2.js

--- Screen2.js
+++ Screen2.js
@@ -1,37 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, ImageBackground, Image, StyleSheet, TouchableOpacity, Animated } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, ImageBackground, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import Animated from 'react-native-reanimated';
+import { Acs } from 'react-native-aos';
 
 const Screen2 = ({ navigation }) => {
-  const [showSecondImage, setShowSecondImage] = useState(false);
   const [showThirdImage, setShowThirdImage] = useState(false);
-  const [slideAnimation] = useState(new Animated.Value(0));
-  const [timer, setTimer] = useState(20000);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTimer((prevTimer) => prevTimer + 20000);
-    }, 20000);
-
-    return () => clearInterval(interval);
-  }, []);
 
   const handleArrowPress = () => {
-    if (!showSecondImage) {
-      setShowSecondImage(true);
-    } else if (!showThirdImage) {
-      setShowThirdImage(true);
-      Animated.timing(slideAnimation, {
-        toValue: 1,
-        duration: 500,
-        useNativeDriver: true,
-      }).start();
-    }
+    setShowThirdImage(true);
+    setTimeout(() => {
+      navigation.navigate('Screen3');
+    }, 1000);
   };
-
-  const slideRight = slideAnimation.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['0%', '100%'],
-  });
 
   return (
     <ImageBackground source={require('./award_bg.png')} style={styles.container}>
@@ -40,24 +20,19 @@ const Screen2 = ({ navigation }) => {
       <View style={styles.contentContainer}>
         <Image source={require('./main-heart.png')} style={styles.imageAbove} resizeMode="contain" />
         <Text style={styles.screenText}>Give U Some Love</Text>
-        {showSecondImage && (
-          <View style={styles.card}>
+        <Animated.View
+          style={[styles.cardContainer, Acs.fadeIn, { transform: [{ translateY: showThirdImage ? 0 : 100 }] }]}
+        >
+          <View style={styles.avtarContainer}>
             <Image source={require('./avtar2.png')} style={styles.avtarImage} resizeMode="contain" />
-            <View style={styles.cardTextContainer}>
-              <Text style={styles.cardText}>D Sally</Text>
-              <Text style={styles.timerText}>{timer} likes</Text>
-            </View>
           </View>
-        )}
-        {showThirdImage && (
-          <Animated.View style={[styles.congratsContainer, { transform: [{ translateX: slideRight }] }]}>
-            <Text style={styles.congratsText}>Congrats!</Text>
-          </Animated.View>
-        )}
+          <View style={styles.cardTextContainer}>
+            <Text style={styles.cardText}>D-Lister</Text>
+            <Text style={styles.cardText}>Sally</Text>
+          </View>
+        </Animated.View>
         <Image source={require('./girlClap.png')} style={styles.image} resizeMode="contain" />
-        {showThirdImage && (
-          <Text style={styles.friendsText}>4 Friends give you some Love</Text>
-        )}
+        {showThirdImage && <Text style={styles.congratsText}>Congrats!</Text>}
         <TouchableOpacity style={styles.arrowContainer} onPress={handleArrowPress}>
           <Image source={require('./arrow.png')} style={styles.arrow} resizeMode="contain" />
         </TouchableOpacity>
@@ -98,7 +73,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     color: 'yellow',
   },
-  card: {
+  cardContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
@@ -109,43 +84,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     borderRadius: 10,
   },
+  avtarContainer: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    overflow: 'hidden',
+  },
   avtarImage: {
-    width: 50,
-    height: 50,
+    width: '100%',
+    height: '100%',
   },
   cardTextContainer: {
     flex: 1,
     marginLeft: 20,
+    justifyContent: 'center',
   },
   cardText: {
     fontSize: 18,
-    color: '#000',
-    textAlign: 'right',
-  },
-  timerText: {
-    fontSize: 16,
-    color: '#666',
-    textAlign: 'right',
-  },
-  congratsContainer: {
-    position: 'absolute',
-    top: 80,
-    right: 20,
-    padding: 10,
-    backgroundColor: 'yellow',
-    borderRadius: 5,
-  },
-  congratsText: {
-    fontSize: 18,
-    color: '#000',
-    fontWeight: 'bold',
+    color: 'purple',
+    fontStyle: 'italic',
   },
   image: {
     width: 200,
     height: 200,
     marginTop: 20,
   },
-  friendsText: {
+  congratsText: {
     fontSize: 18,
     color: 'yellow',
     marginTop: 10,
