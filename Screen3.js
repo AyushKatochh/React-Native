55681180a42f761f1f29821845ab385d1e5acb62
Ayush Katoch <katochayushhp@gmail.com>
2023-05-21 11:32:55 +0530

    Update Screen3.js

--- Screen3.js
+++ Screen3.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, ImageBackground, Image, StyleSheet } from 'react-native';
 
-const Screen3 = ({ navigation }) => {
+const Screen1 = ({ navigation }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      navigation.navigate('Screen2');
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <ImageBackground source={require('./award_bg.png')} style={styles.container}>
       <Image source={require('./curtain.png')} style={[styles.curtain, styles.curtainLeft]} resizeMode="contain" />
       <Image source={require('./curtain.png')} style={[styles.curtain, styles.curtainRight]} resizeMode="contain" />
       <View style={styles.contentContainer}>
-        <Image source={require('./main-heart.png')} style={styles.imageAbove} resizeMode="contain" />
-        <Text style={styles.screenText}>Screen 3</Text>
+        <Image source={require('./castingLogo.png')} style={styles.imageAbove} resizeMode="contain" />
+        <Text style={styles.castingText}>Casting Call</Text>
         <Image source={require('./girlClap.png')} style={styles.image} resizeMode="contain" />
         <Image source={require('./awardPlatform.png')} style={styles.imageBelow} resizeMode="contain" />
-        <Text style={styles.friendsText}>4 Friends give you some Love</Text>
       </View>
     </ImageBackground>
   );
@@ -44,10 +51,12 @@ const styles = StyleSheet.create({
     height: 200,
     marginBottom: 20,
   },
-  screenText: {
-    fontSize: 24,
-    marginBottom: 20,
-    color: 'yellow',
+  castingText: {
+    fontSize: 30,
+    color: 'purple',
+    fontStyle: 'italic',
+    transform: [{ rotate: '-7deg' }],
+    marginTop: 10,
   },
   image: {
     width: 200,
@@ -59,11 +68,6 @@ const styles = StyleSheet.create({
     height: 200,
     marginTop: 20,
   },
-  friendsText: {
-    fontSize: 18,
-    color: 'yellow',
-    marginTop: 10,
-  },
 });
 
-export default Screen3;
+export default Screen1;
